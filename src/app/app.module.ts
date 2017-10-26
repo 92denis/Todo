1f4aa907978d45848fcf16d9b4291654ae50ea01
Denis Sadowskiy <92denis-s@mail.ru>
2017-10-26 19:23:42 +0300

    todoList: Added Material card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { TodoFormComponent } from './todo-form/todo-form.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDatepickerModule, MatInputModule, MatFormFieldModule, MatNativeDateModule, MatToolbarModule, MatButtonModule } from '@angular/material';
+import { MatDatepickerModule, MatInputModule, MatFormFieldModule, MatNativeDateModule, MatToolbarModule, MatButtonModule, MatCardModule } from '@angular/material';
 import { TodoService } from './todo.service';
 
 @NgModule({
@@ -23,6 +23,7 @@ import { TodoService } from './todo.service';
     MatNativeDateModule,
     MatToolbarModule,
     MatButtonModule,
+    MatCardModule,
     MatDatepickerModule
   ],
   providers: [TodoService],
