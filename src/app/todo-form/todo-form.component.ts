edd0d91784ace7cbd52517c3fc40c0cb4cd822ba
Denis Sadowskiy <92denis-s@mail.ru>
2017-11-02 16:28:57 +0300

    changed format minDate

--- src/app/todo-form/todo-form.component.ts
+++ src/app/todo-form/todo-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../todo';
 import { TodoService } from '../todo.service'
-import { NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'todo-form',
@@ -12,13 +12,13 @@ export class TodoFormComponent implements OnInit {
   todos: Todo[];
   newTodo: Todo;
   nameSearch: string;
-  minDate : any ;
+  minDate: any;
 
   constructor(private todoService: TodoService) {
     this.newTodo = new Todo(null, null, false, []);
     this.nameSearch = '';
-    this.minDate =new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate()+'T'+ new Date().getHours()+':'+new Date().getMinutes();
-   
+    this.minDate = new Date().getFullYear() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getDate() + 'T' + new Date().getHours() + ':' + new Date().getMinutes();
+
   }
   getTodos(): void {
     this.todos = this.todoService.getTodos();
