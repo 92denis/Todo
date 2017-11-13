9965365f6b5a187ead83f2a65aa173b93087f053
Denis Sadowskiy <92denis-s@mail.ru>
2017-11-13 13:15:09 +0300

    update pipe Search

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -10,8 +10,13 @@ export class SearchPipe implements PipeTransform {
       return todos;
     } else {
       return todos.filter(todo => {
-        return todo.name.toLowerCase().includes(value.toLowerCase())
-          || todo.tags.some(tag => tag.toLowerCase().includes(value.toLowerCase()));
+        if (todo.tags === undefined ) {
+          return todo.name.toLowerCase().includes(value.toLowerCase());
+        }
+        else {
+          return todo.name.toLowerCase().includes(value.toLowerCase())
+            || todo.tags.some(tag => tag.toLowerCase().includes(value.toLowerCase()));
+        }
       });
     }
   }
