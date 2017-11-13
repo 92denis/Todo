6f8373a0cda75e466bd26afabc9329b23bf73c51
Denis Sadowskiy <92denis-s@mail.ru>
2017-11-13 13:12:25 +0300

    changed class Todo

--- src/app/todo.ts
+++ src/app/todo.ts
@@ -1,7 +1,12 @@
 export class Todo {
-    constructor(public id: number,
-        public name: string,
-        public date: Date,
-        public checked: boolean,
-        public tags: any[]) { }
+    public $key: string;
+    public name: string;
+    public date: Date;
+    public checked: boolean;
+    public tags: any[];
+
+    constructor() {
+        this.checked = false;
+        this.tags = [ ];
+    }
 }
